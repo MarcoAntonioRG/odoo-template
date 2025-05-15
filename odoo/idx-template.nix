609fd4db9d938b8c42d4de39a781379f05e64cd9
Marco Román <marcoroman199@gmail.com>
2025-05-15 16:59:59 -0400

    Create dev.nix and update idx-template.nix

--- odoo/idx-template.nix
+++ odoo/idx-template.nix
@@ -1,40 +1,31 @@
-# Originally part of the project idx under the Apache 2.0 license
-# Modified by Marco Román on 05-15-2025
-
 { pkgs, version ? "17", ... }: {
   packages = [
     pkgs.git
   ];
 
   bootstrap = let
-    packagesV15 = [
-      pkgs.gcc
-      pkgs.python39
-      pkgs.python39Packages.pip
-      pkgs.python39Packages.setuptools
-      pkgs.python39Packages.virtualenv
-      pkgs.openldap
-      pkgs.openldap.dev
-      pkgs.cyrus_sasl.dev
-      pkgs.libpqxx
-      pkgs.libxml2.dev
-      pkgs.libxslt.dev
-    ];
-    packagesV16 = [
-      pkgs.gcc
-      pkgs.python311
-      pkgs.python311Packages.pip
-      pkgs.python311Packages.setuptools
-      pkgs.python311Packages.virtualenv
-      pkgs.openldap
-      pkgs.openldap.dev
-      pkgs.cyrus_sasl.dev
-      pkgs.libpqxx
-    ];
-    packagesV17 = packagesV16;
+    odoo = rec {
+      packagesV15 = [
+        "pkgs.gcc"
+        "pkgs.python39"
+        "pkgs.openldap"
+        "pkgs.openldap.dev"
+        "pkgs.cyrus_sasl.dev"
+        "pkgs.libpqxx"
+        "pkgs.libxml2.dev"
+        "pkgs.libxslt.dev"
+      ];
 
-    odooPackages = builtins.getAttr ("packagesV" + version) {
-      inherit packagesV15 packagesV16 packagesV17;
+      packagesV16 = [
+        "pkgs.gcc"
+        "pkgs.python311Full"
+        "pkgs.openldap"
+        "pkgs.openldap.dev"
+        "pkgs.cyrus_sasl.dev"
+        "pkgs.libpqxx"
+      ];
+
+      packagesV17 = packagesV16;
     };
   in
     ''
@@ -44,63 +35,18 @@
       mkdir -p .idx/.data/
       mkdir -p .vscode/
 
-      cat > .idx/dev.nix <<EOF
-{ pkgs, ... }: {
-  channel = "stable-24.05";
-  packages = [
-    ${builtins.concatStringsSep "\n    " (map (p: "pkgs.${p.name or "unknown"}") odooPackages)}
-  ];
-  services.postgres.enable = true;
-
-  idx = {
-    extensions = [
-      "ms-python.python"
-    ];
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["sh" "httpserver.sh" "\$PORT"];
-          manager = "web";
-        };
-      };
-    };
-    workspace = {
-      onCreate = {
-        odoo-install = \'\'
-          set -e
-          echo "✅ Creando entorno virtual..."
-          python -m venv .venv
-          source .venv/bin/activate
-          pip install --upgrade pip
-          pip install -r .idx/.data/odoo/requirements.txt
-
-          BIN_PATH="\$(pwd)/.idx/.data/odoo/odoo-bin"
-          if [ -f "\$BIN_PATH" ]; then
-            ln -s "\$BIN_PATH" .venv/bin/odoo-bin
-          fi
-
-          odoo-bin --save --stop-after-init
-          mv ../.odoorc odoo.conf
-
-          sed -i \
-            -e "/^addons_path =/ s|$|,\$(pwd)/custom_addons|" \
-            -e "s|.local/share/Odoo|.idx/.data/odoo-data|g" \
-            odoo.conf
-        \'\';
-        default.openFiles = [ "README.md" ];
-      };
-    };
-  };
-}
-EOF
-
+      cp -f ${./dev.nix} ".idx/dev.nix"
       cp -f ${./httpserver.sh} "httpserver.sh"
       cp -f ${./README.md} "README.md"
       cp -f ${./settings.json} ".vscode/settings.json"
       printf "/.idx/odoo\n/.idx/odoo-data" > ".gitignore"
 
       git clone https://github.com/odoo/odoo.git --single-branch --branch ${version}.0 --depth 1 .idx/.data/odoo
+
+      sed -i                                                                                  \
+          -e "s/\$WS_NAME/$WS_NAME/g"                                                         \
+          -e 's/$PACKAGES/${builtins.concatStringsSep "\\\n    " odoo.${"packagesV" + version}}/g' \
+          .idx/dev.nix
 
       mkdir custom_addons/
 
